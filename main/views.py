3846b608f5f682fed33329a169709fd21c908824
Abzhik <52734118+Abjik@users.noreply.github.com>
2023-07-18 00:05:37 +0600

    start

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("<h4>Home Page</h4>")
+    return render(request, 'main/index.html')
 
 def login(request):
-    return HttpResponse("<h4>Login Page</h4>")
+    return render(request, 'main/login.html')
 
 def reg(request):
-    return HttpResponse("<h4>Registr Page</h4>")
+    return render(request, 'main/reg.html')
